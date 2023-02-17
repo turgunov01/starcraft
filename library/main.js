c7b5eb8f8d9e02917c1648b194a246c2c1f7108f
Ваше Имя <you@example.com>
2023-02-17 21:08:27 +0500

    save & test

--- library/main.js
+++ library/main.js
@@ -1,21 +1,8 @@
-function displayImages() {
-    let images = ""
-    imagesArray.forEach((image, index) => {
-        images += `<div class="show-sponsor">
-                <img src="${URL.createObjectURL(image)}" alt="image">
-                <span onclick="deleteImage(${index})">&times;</span>
-              </div>`
+let dropdown = document.querySelectorAll('.dropdown-open')
+for (let i = 0; i < dropdown.length; i++) {
+    let dropdownOpen = dropdown[i].parentElement
+    dropdown[i].addEventListener('click', function (e) {
+        dropdownOpen.classList.toggle('visible')
     })
-    output.innerHTML = images
 }
 
-
-let input = document.querySelector('#custom-field')
-let output = document.querySelector('output')
-let imagesArray = []
-
-input.addEventListener("change", () => {
-    const file = input.files
-    imagesArray.push(file[0])
-    displayImages()
-})
